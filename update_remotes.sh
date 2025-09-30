75dbe2c20579391c3eb376abc6a52f228ec66a0e
Kitlith <kitlith@kitl.pw>
2025-09-29 17:11:32 -0700

    Make update script abandon old merge parents

--- update_remotes.sh
+++ update_remotes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 maybe_create_remote() {
-	remote_name="$1"
-	remote_url="$2"
+	local remote_name="$1"
+	local remote_url="$2"
 
 	if jj git remote list | cut -d ' ' -f 1 | grep -q "$remote_name"; then
 		: # remote already exists, do nothing
@@ -14,18 +14,13 @@ maybe_create_remote() {
 }
 
 filter_remote() {
-	remote_name="$1"
-	remote_branch="$2"
-	filter_file="$3"
-	shift 2
+	local remote_name="$1"
+	local remote_branch="$2"
+	local filter_file="$3"
 	echo "-- Fetching upstream $remote_name"
 	jj git fetch --remote "$remote_name" -b "$remote_branch"
 	echo "-- Updating $remote_name subset"
-	josh-filter --update refs/heads/${remote_name}-subset-tmp dummy-not-filter --file "$filter_file" refs/remotes/${remote_name}/${remote_branch}
-	# ran into issues when I tried to update the branch directly, though I need to re-check
-	# it might've been my editor trying to do things at the same time.
-	jj bookmark move --allow-backwards ${remote_name}-subset --to ${remote_name}-subset-tmp
-	jj bookmark delete ${remote_name}-subset-tmp
+	josh-filter --update refs/heads/${remote_name}-subset dummy-not-filter --file "$filter_file" refs/remotes/${remote_name}/${remote_branch}
 }
 
 maybe_create_remote dotnet https://github.com/dotnet/runtime.git
@@ -34,4 +29,9 @@ maybe_create_remote msquic https://github.com/microsoft/msquic.git
 filter_remote msquic main workflow/msquic-filter
 
 echo "-- Rebasing onto updated upstreams"
-jj rebase -s merge -d workflow -d dotnet-subset -d msquic-subset
+abandon_parents=$(jj log --no-graph -T 'change_id ++ " | "' -r 'merge- ~ workflow::')
+workflow_parent=$(jj log --no-graph -T 'change_id ++ " | "' -r 'merge- & workflow::')
+jj rebase -s merge -d "$workflow_parent none()" -d dotnet-subset -d msquic-subset
+
+echo "-- Abandoning old versions of upstreams"
+jj abandon -r "..($abandon_parents none()) ~ ..merge"
